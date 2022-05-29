b45fd0c71aa994bf8580c1c47e9ec16da4476158
lkindere <lkindere@student.42heilbronn.de>
2022-05-29 02:35:02 +0200

    Signal working on linux, rl_replace_line was broken on mac something to do with includes, check slack

--- main/signal.c
+++ main/signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmeising <mmeising@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lkindere <lkindere@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 18:43:34 by lkindere          #+#    #+#             */
-/*   Updated: 2022/05/26 17:55:50 by mmeising         ###   ########.fr       */
+/*   Updated: 2022/05/29 02:34:20 by lkindere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@ void    handle_sigint(int sig)
 {
 	(void)sig;
     write(1, "\n", 1);
-    write(1, "MiniJunk XD: ", 13);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
 void    signal_handler(void)
 {
+	struct termios		term;
 	struct sigaction	sint;
 	struct sigaction	squit;
 
+	tcgetattr(0, &term);
+	term.c_lflag &= ~ECHOCTL;
+	tcsetattr(0, 0, &term);
 	sint.sa_handler = &handle_sigint;
 	sint.sa_flags = SA_RESTART;
 	squit.sa_handler = SIG_IGN;

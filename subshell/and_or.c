aec4864e864f4f5c168702af93af2648dc7db378
Lukas Kinderevicius <lkindere@2-F-9.42heilbronn.de>
2022-05-31 17:44:24 +0200

    test

--- subshell/and_or.c
+++ subshell/and_or.c
@@ -6,7 +6,7 @@
 /*   By: lkindere <lkindere@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 08:53:41 by lkindere          #+#    #+#             */
-/*   Updated: 2022/05/31 11:57:52 by lkindere         ###   ########.fr       */
+/*   Updated: 2022/05/31 17:14:50 by lkindere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,21 +38,8 @@ static void	check_exit(t_data *data, char **segment, int and_or)
 	terminator(&data);
 }
 
-//Checks the remainder of segment for separators
-//Removes || and && setting and_or next
-//Returns 0 on success
-//Returns 1 on error
-int	handle_and_or(t_data *data, char **segment, int *and_or)
+int	check_separators(t_data *data, char **segment, int *and_or, int i)
 {
-	int	i;
-
-	i = 0;
-	if (!(*segment))
-		return (0);
-	while ((*segment)[i] && !is_separator((*segment)[i]))
-		i++;
-	if (!(*segment)[i])
-		return (0);
 	if (first_sep(&((*segment)[i + 2])) == '(')
 		return (0);
 	if ((*segment)[i] == '&' && (*segment)[i + 1] == '&')
@@ -67,6 +54,35 @@ int	handle_and_or(t_data *data, char **segment, int *and_or)
 		check_exit(data, segment, *and_or);
 		return (remove_separator(segment, i));
 	}
+	return (0);
+}
+
+//Checks the remainder of segment for separators
+//Removes || and && setting and_or next
+//Returns 0 on success
+//Returns 1 on error
+int	handle_and_or(t_data *data, char **segment, int *and_or)
+{
+	t_flags	flag;
+	int		i;
+
+	i = 0;
+	flag.single_quote = 0;
+	flag.double_quote = 0;
+	if (!(*segment))
+		return (0);
+	while ((*segment)[i])
+	{
+		if (is_separator((*segment)[i]) && !flag.single_quote && !flag.double_quote)
+			return (check_separators(data, segment, and_or, i));
+		if ((*segment)[i] == '\'')
+			flag.single_quote = ~flag.single_quote & 1;
+		if ((*segment)[i] == '"')
+			flag.double_quote = ~flag.double_quote & 1;
+		i++;
+	}
+	if (!(*segment)[i])
+		return (0);
 	if (data->is_fork)
 		data->start_code = -1;
 	return (0);
